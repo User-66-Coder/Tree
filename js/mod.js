086db52490ccd67904df95e6194867d912f5df4e
Jun <gorjunleong@gmail.com>
2024-02-29 18:07:09 -0500

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -1,7 +1,7 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
+	name: "The Tree",
+	id: "h&9jH6^gdS",
+	author: "",
 	pointsName: "points",
 	modFiles: ["layers.js", "tree.js"],
 
@@ -13,14 +13,13 @@ let modInfo = {
 
 // Set your version in num and name
 let VERSION = {
-	num: "0.0",
-	name: "Literally nothing",
+	num: "0.0.0",
+	name: "Nothing",
 }
 
 let changelog = `<h1>Changelog:</h1><br>
-	<h3>v0.0</h3><br>
-		- Added things.<br>
-		- Added stuff.`
+	<h3>v0.0.0</h3><br>
+		- Changed Info`
 
 let winText = `Congratulations! You have reached the end and beaten this game, but for now...`
 
